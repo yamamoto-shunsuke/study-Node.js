a0beee8f2b751d35f41d070fe0d7c1eea6a66181
yamamoto-shunsuke <72194757+yamamoto-shunsuke@users.noreply.github.com>
2020-10-05 18:08:11 +0900

    Webアプリケーション完成版(Node.js基礎学習)

--- nodecli/index.js
+++ nodecli/index.js
@@ -1,10 +1,11 @@
-var server = require("./server");//requireし、変数に割り当てることでサーバ起動関数を利用できる。
-var router = require("./router");//route関数をサーバに組み込む
+var server = require("./server");
+var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
--- nodecli/server.js
+++ nodecli/server.js
@@ -1,14 +1,42 @@
 //サーバ起動スクリプト
 
-var http = require("http");//httpモジュールをローカル変数に割り当てている
-var url = require("url");//urlモジュール：URLの各部分(リクエストパスやクエリ文字列)を抽出するメソッドを提供する。
+// var http = require("http");//httpモジュールをローカル変数に割り当てている
+// var url = require("url");//urlモジュール：URLの各部分(リクエストパスやクエリ文字列)を抽出するメソッドを提供する。
 
-function start(route, handle) {//start関数(今回だとサーバ起動スクリプト)exportすることで他ファイルの関数が利用可能。
+// function start(route, handle) {//start関数(今回だとサーバ起動スクリプト)exportすることで他ファイルの関数が利用可能。
+//   function onRequest(request, response) {
+//     var postData = "";
+//     var pathname = url.parse(request.url).pathname;
+//     console.log("Request for " + pathname + " received.");//ブラウザがファビコンをロードするため、二回出力。
+
+//     request.setEncoding("utf8");
+
+//     request.addListener("data", function(postDataChunk) {
+//       postData += postDataChunk;
+//       console.log("Received POST data chunk '"+
+//       postDataChunk + "'.");
+//     });
+
+//     request.addListener("end", function() {
+//       route(handle, pathname, response, postData);
+//     });
+
+//   }
+
+//   http.createServer(onRequest).listen(8888);
+//   console.log("Server has started.");
+// }
+
+// exports.start = start;
+
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");//ブラウザがファビコンをロードするため、二回出力。
-
-    route(handle, pathname, response);
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
